ccb260589ac6bc290ccb6cfaa3305b875f970330
DavidHdw <tossboydavid@sina.com>
2014-11-17 15:37:50 +0800

    basic upload by carrierwave

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :paintings
+  root 'paintings#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
